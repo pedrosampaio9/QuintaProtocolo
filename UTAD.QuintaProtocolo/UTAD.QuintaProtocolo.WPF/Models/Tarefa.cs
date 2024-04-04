bd8eec07cf892e45ed536c61db3d24fe3d3da480
pedrosampaio9 <al79431@alunos.utad.pt>
2024-04-04 18:29:32 +0100

    ´Tudo Feito

---
+++ UTAD.QuintaProtocolo/UTAD.QuintaProtocolo.WPF/Models/Tarefa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTAD.QuintaProtocolo.WPF.Models
+{
+    public abstract class Tarefa
+    {
+        protected Tarefa()
+        {
+
+            if (string.IsNullOrEmpty(Periodicidade))
+                Periodicidade = Guid.NewGuid().ToString();
+
+        }
+        public string Periodicidade { get; set; }
+
+        public string Titulo { get; set; }
+        public string Descrição { get; set; }
+        public IList<string> DataInicio { get; set; }
+
+        public IList<string> DataTérmino { get; set; }
+
+        public string NivelImportância { get; set; }
+
+        public string Estado { get; set; }
+
+        public string AlertaAntecipação { get; set; }
+
+        public string AlertaExecução { get; set; }
+
+
+
+
+
+
+    }
+}
